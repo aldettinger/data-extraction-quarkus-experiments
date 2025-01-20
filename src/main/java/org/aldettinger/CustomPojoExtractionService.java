f47120654283810abb8adb4d5e47a7d1f0146692
aldettinger <aldettinger@gmail.com>
2025-01-20 17:08:51 +0100

    Workaround LocalDate deserialization issue

--- src/main/java/org/aldettinger/CustomPojoExtractionService.java
+++ src/main/java/org/aldettinger/CustomPojoExtractionService.java
@@ -1,5 +1,7 @@
 package org.aldettinger;
 
+import java.time.Month;
+
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import io.quarkus.runtime.annotations.RegisterForReflection;
@@ -13,13 +15,23 @@ public interface CustomPojoExtractionService {
     static class CustomPojo {
         public boolean customerSatisfied;
         public String customerName;
-        public String customerBirthday;
+        public CustomDate customerBirthday;
         public String summary;
+    }
+
+    @RegisterForReflection
+    static class CustomDate {
+        int year;
+        int month;
+        int day;
+
+        public String toString() {
+            return String.format("%d %s %d", day, Month.of(month), year);
+        }
     }
 
     static final String CUSTOM_POJO_EXTRACT_PROMPT
             = "Extract information about a customer from the text delimited by triple backticks: ```{text}```."
-              + "The customerBirthday field should be formatted as YYYY-MM-DD."
               + "The summary field should concisely relate the customer main ask.";
 
     @UserMessage(CUSTOM_POJO_EXTRACT_PROMPT)
